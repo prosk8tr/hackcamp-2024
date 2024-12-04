3fc4e36c36e4345761538c2b84d861a146aa97cf
Keron-UoS <k.bland1@edu.salford.ac.uk>
2024-12-04 13:34:38 +0000

    Add files via upload
    Project folder containing templates for website

---
+++ fish/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, redirect 
+
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return redirect("/home")
+
+@app.route('/home')
+def home():
+    return render_template("index.html")
+    
+@app.route('/page1')
+def page1():
+    return render_template("page1.html")
+
+@app.route('/comparison')
+def comparison():
+    return render_template("comparison.html")
