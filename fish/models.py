8d9d99153d12c06dac61ed635155afaff923132e
prosk8tr <e.collier2@edu.salford.ac.uk>
2024-12-06 16:20:11 +0000

    adding database object classes

--- fish/models.py
+++ fish/models.py
@@ -2,16 +2,85 @@ import os
 import psycopg
 
 class db_connect:
-    #Class variables are shared between every instance of a class.
-    url = os.getenv("DATABASE_URL") #Get the URL of the database in the .env config
-    connection = psycopg.connect(url) #Open connection to the database
-    cursor = connection.cursor() #This is what allows select queries
+    def __init__(self):
+        #Class variables are shared between every instance of a class.
+        self.url = os.getenv("DATABASE_URL") #Get the URL of the database in the .env config
+        self.connection = psycopg.connect(self.url) #Open connection to the database
+        self.cursor = self.connection.cursor() #This is what allows select queries
 
-    def get_repos(self):
-        SQL = """SELECT * FROM repositories;"""
+    def get_all_repositories(self): #Used to get list of repositories for the view
+        SQL = """SELECT id, name, owner FROM repositories;"""
         self.cursor.execute(SQL)
-        repos = self.cursor.fetchall()
-        results = []
-        for repo in repos:
-            results.append(repo)
-        return results    
+        results = self.cursor.fetchall() #Do query
+
+        all_repositories = [] #Array of objects
+
+        for result in results:
+            row = [result[0],result[1],result[2]] #Make a list with the required values for the object attributes
+            new_repo = repo(*row) #Make a repository object 
+            print(new_repo.get_name()+new_repo.get_id()+new_repo.get_owner())
+            all_repositories.append(new_repo)
+        return all_repositories
+
+        def get_files_from_repo(repo_name):
+            pass
+
+class repo:
+    def __init__(self, repo_id, name, owner):
+        self.repo_id = repo_id
+        self.name = name
+        self.owner = owner
+    
+    def get_id(self):
+        return self.repo_id
+    
+    def get_name(self):
+        return self.name
+
+    def get_owner(self):
+        return self.owner
+
+class repo_file:
+    def __init__(self, file_id, path, name, line_count, functional_line_count):
+        self.file_id = file_id
+        self.path = path
+        self.name = name
+        self.line_count = line_count
+        self.functional_line_count = functional_line_count
+    
+    def get_id(self):
+        return self.file_id
+
+    def get_path(self):
+        return self.path
+
+    def get_name(self):
+        return self.name
+
+    def get_line_count(self):
+        return self.line_count
+
+    def get_functional_line_count(self):
+        return self.functional_line_count
+
+class commit:
+    def __init__(self, commit_id, name, author, repository_id):
+        self.commit_id = commit_id
+        self.name = name
+        self.author = author
+        self.repository_id = repository_id
+    
+    def get_commit_id(self):
+        return self.commit_id
+    
+    def get_name(self):
+        return self.name
+    
+    def get_author(self):
+        return self.author
+    
+    def get_repository_id(self):
+        return repository_id
+
+
+
